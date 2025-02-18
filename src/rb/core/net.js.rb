3d87578857196e6d9537d4e49303fa0f2effd757
Filip Vrba <filipvrba@pm.me>
2025-02-18 22:00:20 +0100

    add bootstrap and bef api

--- src/rb/core/net.js.rb
+++ src/rb/core/net.js.rb
@@ -7,6 +7,41 @@ export default class Net
     .then(lambda do |text|
       callback(text) if callback
     end)
+    .catch(lambda do
+      callback(nil) if callback
+    end)
+  end
+
+  def self.bef(query, &callback)
+    encode_query = encodeURIComponent(query)
+    Net.curl("/api/bef?query=#{encode_query}") do |response|
+
+      if response
+        data = JSON.parse(response)
+        if data['status_code'] == 403 || data['status_code'] == 405 ||
+          data.status == 'SQL Error'
+          
+          console.warn(data)
+          callback(nil) if callback
+        else
+          callback(data) if callback
+        end
+      else
+        callback(nil) if callback
+      end
+
+    end
+  end
+
+  def self.check_internet(&callback)
+    health_url = 'https://bef.fly.dev/health'
+
+    fetch(health_url, { method: 'GET' })
+    .then(lambda do |response|
+      callback(true) if callback
+    end)
+    .catch(lambda do
+      callback(false) if callback
+    end)
   end
 end
-window.Net = Net
