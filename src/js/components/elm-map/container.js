287f99a8cf12e5834a67b10c410463c3c4d7029d
Filip Vrba <filipvrba@pm.me>
2025-02-19 01:24:11 +0100

    change style map

--- src/js/components/elm-map/container.js
+++ src/js/components/elm-map/container.js
@@ -9,9 +9,9 @@ export default class CContainer {
       states.csCenter.zoom
     );
 
-    let options = {attribution: "&copy; <a href='https://www.stadiamaps.com/' target='_blank'>Stadia Maps</a> &copy; <a href='https://openmaptiles.org/' target='_blank'>OpenMapTiles</a> &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"};
+    let options = {attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/'>Carto</a>"};
     L.tileLayer(CContainer.TILE_LAYER, options).addTo(this._lMap)
   }
 };
 
-CContainer.TILE_LAYER = "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+CContainer.TILE_LAYER = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
--- src/rb/components/elm-map/container.js.rb
+++ src/rb/components/elm-map/container.js.rb
@@ -1,12 +1,12 @@
 export default class CContainer
-  TILE_LAYER = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+  TILE_LAYER = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
 
   attr_reader :l_map
 
   def initialize(states)
     @l_map    = L.map('emContainer').set_view(states.cs_center.geo, states.cs_center.zoom)
     options = {
-      attribution: "&copy; <a href='https://www.stadiamaps.com/' target='_blank'>Stadia Maps</a> &copy; <a href='https://openmaptiles.org/' target='_blank'>OpenMapTiles</a> &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+      attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/'>Carto</a>",
     }
 
     L.tile_layer(TILE_LAYER, options).add_to(@l_map)
